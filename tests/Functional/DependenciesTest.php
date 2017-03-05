cf7dfc337872cd5dba92b745d5595406c165bc43
brunty <matt@mfyu.co.uk>
2017-03-05 11:31:22 +0000

    Add tests in for depdencies in container

--- tests/Functional/DependenciesTest.php
+++ tests/Functional/DependenciesTest.php
@@ -2,10 +2,23 @@
 
 namespace Tests\Functional;
 
+use App\Domain\Event\EventDispatcher;
+use App\Domain\Repository\CageRepository;
+use App\Http\Action\Page\HomepageAction;
+use App\Http\Action\RandomCage\MultipleImageAction;
+use App\Http\Action\RandomCage\SingleImageAction;
+use App\Http\Negotiator\AcceptHeaderNegotiator;
+use App\Http\Responder\Page\Homepage\HomepageResponder;
 use App\Http\Responder\RandomCage\MultipleImage\MultipleImageResponder;
+use App\Http\Responder\RandomCage\SingleImage\SingleImageResponder;
+use App\Presentation\Page\Homepage\ContentCreator as HomepageContentCreator;
+use App\Presentation\RandomCage\SingleImage\ContentCreator as SingleImageContentCreator;
+use App\Presentation\RandomCage\MultipleImage\ContentCreator as MultipleImageContentCreator;
 use Interop\Container\ContainerInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 use Slim\App;
+use Slim\Views\Twig;
 
 class DependenciesTest extends TestCase
 {
@@ -42,7 +55,19 @@ class DependenciesTest extends TestCase
     public function provider_for_it_resolves_the_correct_things_from_the_container(): array
     {
         return [
-            ['app.random_cage.multiple_image_responder', MultipleImageResponder::class]
+            ['app.random_cage.multiple_image_responder', MultipleImageResponder::class],
+            ['view', Twig::class],
+            ['logger', LoggerInterface::class],
+            ['app.repository.cage', CageRepository::class],
+            ['app.event_dispatcher', EventDispatcher::class],
+            ['app.accept_header_negotiator', AcceptHeaderNegotiator::class],
+            ['app.homepage_content_creator', HomepageContentCreator::class],
+            ['app.page.homepage_responder', HomepageResponder::class],
+            ['app.http.page.homepage', HomepageAction::class],
+            ['app.random_cage.single_image_responder', SingleImageResponder::class],
+            ['app.http.random_cage.single_image', SingleImageAction::class],
+            ['app.random_cage.multiple_image_responder', MultipleImageResponder::class],
+            ['app.http.random_cage.multiple_image', MultipleImageAction::class],
         ];
     }
 }
